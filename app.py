388947c987fb5375dc781f1085e03e782ed0add8
Zijie Dong <z68250532@gmail.com>
2021-09-24 21:30:34 -0400

    initial project

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
+import json
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home')
 def hello_world():
-    return 'Hello World!'
+    return '<h1>Home</h1>'
+
+
+@app.route('/user')
+def user():
+    return json.dumps({'name': 'alice', 'email': 'alice@outlook.com'})
 
 if __name__ == '__main__':
     app.run()
